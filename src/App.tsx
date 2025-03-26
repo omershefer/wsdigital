f0ebf1173b0e613dfda448b822f217bfba859754
Omer Sheffer <omersheffer@MacBook-Air-sl-Omer.local>
2025-03-26 12:07:35 +0200

    fixing url problems

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Suspense } from "react";
 import { init } from "@emailjs/browser";
 import "./App.css";
-
-// import scrolling component
 import ScrollToTopOnRouteChange from "./components/general/ScrollToTopOnRouterChange";
-
-// Import page components
 import Home from "./pages/Home";
 import PageNotFound from "./pages/PageNotFound";
 import AboutUs from "./pages/AboutUs";
@@ -16,35 +12,25 @@ import Contactpage from "./pages/ContactPage";
 import WorkExamples from "./pages/WorkExamples";
 
 function App() {
-  const urlBase = "/wsdigital"
+  const urlBase = "/wsdigital";
   init("Osm30QnSHmceSTNsD");
+
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={urlBase}>
       <div className="w-full h-full">
         <ScrollToTopOnRouteChange />
         <Suspense fallback={<div>Loading...</div>}>
           <Routes>
-            <Route path={`${urlBase}/home`} element={<Home />} />
-            <Route
-              path={`${urlBase}`}
-              element={<Navigate to={`${urlBase}/home`} replace />}
-            />
-            <Route
-              path={`${urlBase}/`}
-              element={<Navigate to={`${urlBase}/home`} replace />}
-            />
-            <Route
-              path={`/`}
-              element={<Navigate to={`${urlBase}/home`} replace />}
-            />
-            <Route path={`${urlBase}/aboutus`} element={<AboutUs />} />
-            <Route path={`${urlBase}/contact`} element={<Contactpage />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/" element={<Navigate to="/home" replace />} />
+            <Route path="/aboutus" element={<AboutUs />} />
+            <Route path="/contact" element={<Contactpage />} />
             <Route
               path="/accessibility"
               element={<AccessibilityDecleration />}
             />
-            <Route path={`${urlBase}/pricing`} element={<Pricing />} />
-            <Route path={`${urlBase}/catalog`} element={<WorkExamples />} />
+            <Route path="/pricing" element={<Pricing />} />
+            <Route path="/catalog" element={<WorkExamples />} />
             <Route path="*" element={<PageNotFound />} />
           </Routes>
         </Suspense>
